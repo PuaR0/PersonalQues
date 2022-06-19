224ac25513857c457fae8b5a457aa8b3740ced6d
PuaR0 <92799139+PuaR0@users.noreply.github.com>
2022-06-19 06:29:34 +0300

    Initial Commit

---
+++ PersonalQues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQues
+//
+//  Created by Алиага С on 19.6.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
